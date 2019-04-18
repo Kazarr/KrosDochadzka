8d6d62e7c81a52de3e42237bcbaeb7d6298ced1f
JakubCelovsky <celovsky.jakubmail.com>
2019-04-18 11:05:43 +0200

    +manager repo

---
+++ AttendanceTermianal/Data/Repository/ManagerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class ManagerRepository
+    {
+        public static DialyResultRepository dialyResultRepository = new DialyResultRepository();
+        public static EmployeeRepository employeeRepository = new EmployeeRepository();
+        public static PersonRepository personRepository = new PersonRepository();
+        public static WorkTypeRepository workTypeRepository = new WorkTypeRepository();
+    }
+}
