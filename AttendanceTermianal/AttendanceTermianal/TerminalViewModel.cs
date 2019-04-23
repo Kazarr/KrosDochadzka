992c7a5888102ead642dce52ed655dfd4a97b913
JakubCelovsky <celovsky.jakubmail.com>
2019-04-23 13:11:08 +0200

    lunch function

--- AttendanceTermianal/AttendanceTermianal/TerminalViewModel.cs
+++ AttendanceTermianal/AttendanceTermianal/TerminalViewModel.cs
@@ -11,6 +11,7 @@ namespace AttendanceTermianal
     public class TerminalViewModel
     {
         private DailyResult _result = new DailyResult();
+        private Empolyee _empolyee = new Empolyee();
         public string CurrentDate()
         {
             string date = DateTime.Now.ToString("dd/MM/yyyy");
@@ -43,20 +44,40 @@ namespace AttendanceTermianal
         public Tuple<bool, int> IsCorrectId(string input)
         {
             int Id = 0;
-            bool isOk = int.TryParse(input, out Id);
-            if (isOk)
+            bool isOk = false;
+            if (!isOk)
             {
+                isOk = int.TryParse(input, out Id); 
                 return new Tuple<bool, int>(true, Id);
             }
             return new Tuple<bool, int>(false, Id);
         }
 
+        public bool CorrectEmp(string input)
+        {
+            try
+            {
+                if (ManagerRepository.EmployeeRepository.GetEmpolyeeByID(int.Parse(input)).Id.Equals(int.Parse(input)))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }               
+            }
+            catch (FormatException)
+            {
+                return false;
+            }      
+        }
+
+
         public void StartWork(int id_employee, int id_worktype)
         {
             _result.IdWorktype = id_worktype;
             _result.IdEmployee = id_employee;
-            _result.Id = ManagerRepository.DailyResultRepository.InsertDialyResult(_result);
-            
+            _result.Id = ManagerRepository.DailyResultRepository.InsertDialyResult(_result);           
         }
         public bool FinishWork(int id_employee)
         {
