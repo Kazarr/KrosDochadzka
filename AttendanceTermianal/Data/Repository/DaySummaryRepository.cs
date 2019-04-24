14e670bc0309586310c163daa1d3b07919d6e530
Michal Matis <miso.matis@gmail.com>
2019-04-24 13:04:58 +0200

    Overview almost done

--- AttendanceTermianal/Data/Repository/DaySummaryRepository.cs
+++ AttendanceTermianal/Data/Repository/DaySummaryRepository.cs
@@ -96,9 +96,60 @@ namespace Data.Repository
 
             daySummary.WorkArrivalTime = GetArrivalTime(date, idEmployee);
             daySummary.WorkLeavingTime = GetLeavingTime(date, idEmployee);
+            daySummary.LunchBreak = GetTimeSpendOnDailyResults(date, idEmployee, 2);
+            daySummary.HolidayTime = GetTimeSpendOnDailyResults(date, idEmployee, 3);
+            daySummary.HomeOffice = GetTimeSpendOnDailyResults(date, idEmployee, 4);
+            daySummary.BusinessTrip = GetTimeSpendOnDailyResults(date, idEmployee, 5);
+            daySummary.Doctor = GetTimeSpendOnDailyResults(date, idEmployee, 6);
+            daySummary.Private = GetTimeSpendOnDailyResults(date, idEmployee, 7);
+            daySummary.Other = GetTimeSpendOnDailyResults(date, idEmployee, 8);
 
             return daySummary;
 
+        }
+
+        private TimeSpan GetTimeSpendOnDailyResults(DateTime date, int IdEmployee, int idWorkType)
+        {
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandText = @"select dr.Start,dr.Finish from DailyResult as dr
+                                                where dr.IdEmployee=@IdEmployee  and dr.IdWorktype=@idWorkType 
+                                                            and convert(date,dr.start)=convert(date,@date)";
+
+                        command.Parameters.Add("@idEmployee", SqlDbType.Int).Value = IdEmployee;
+                        command.Parameters.Add("@idWorkType", SqlDbType.Int).Value = idWorkType;
+                        command.Parameters.Add("@date", SqlDbType.DateTime2).Value = date;
+                        TimeSpan totalTimeWasted = TimeSpan.Zero;
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                if (!reader.IsDBNull(0) && !reader.IsDBNull(1))
+                                {
+                                    totalTimeWasted += (reader.GetDateTime(1) - reader.GetDateTime(0));
+                                }
+                            }
+                            return totalTimeWasted;
+                        }
+
+
+;
+
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                    throw e;
+                }
+            }
         }
 
         public List<DaySummary> GetSummariesByMonth (string month, int idEmployee)
