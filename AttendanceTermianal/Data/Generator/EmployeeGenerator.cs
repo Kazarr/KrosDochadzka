c4cb2f95bb136702b80c83236e0d7e3229afb0aa
JakubCelovsky <celovsky.jakubmail.com>
2019-04-26 13:01:52 +0200

    coment

--- AttendanceTermianal/Data/Generator/EmployeeGenerator.cs
+++ AttendanceTermianal/Data/Generator/EmployeeGenerator.cs
@@ -11,35 +11,35 @@ namespace Data.Generator
 {
     public class EmployeeGenerator
     {
-        private EmployeeRepository _employeeRepository;
-        private PersonRepository _personRepository;
+        //private EmployeeRepository _employeeRepository;
+        //private PersonRepository _personRepository;
 
-        Random r = new Random();
+        //Random r = new Random();
 
-        private DateTime RandomDate()
-        {
-            DateTime start = new DateTime(1995, 1, 1);
-            int range = (DateTime.Today - start).Days;
-            return start.AddDays(r.Next(range));
-        }
+        //private DateTime RandomDate()
+        //{
+        //    DateTime start = new DateTime(1995, 1, 1);
+        //    int range = (DateTime.Today - start).Days;
+        //    return start.AddDays(r.Next(range));
+        //}
 
-        public Empolyee SetEmployee(int i)
-        {
+        //public Empolyee SetEmployee(int i)
+        //{
            
-                string pass = "0000";
-                int idPerson = i;
-                int idPermission = 2;
-                decimal salary = r.Next(1400, 2300);
-                DateTime date = RandomDate();
-                return new Empolyee(pass, idPerson, idPermission, salary, date);            
-        }
-        public void GenerateEmployee()
-        {
-            List<Person> people = ManagerRepository.PersonRepository.GetPersons().ToList();
-            for (int i = 3; i < people.Count-700; i++)
-            {
-                _employeeRepository.InsertFullEmployee(SetEmployee(i));
-            }
-        }
+        //        string pass = "0000";
+        //        int idPerson = i;
+        //        int idPermission = 2;
+        //        decimal salary = r.Next(1400, 2300);
+        //        DateTime date = RandomDate();
+        //        return new Empolyee(pass, idPerson, idPermission, salary, date);            
+        //}
+        //public void GenerateEmployee()
+        //{
+        //    List<Person> people = ManagerRepository.PersonRepository.GetPersons().ToList();
+        //    for (int i = 3; i < people.Count-700; i++)
+        //    {
+        //        _employeeRepository.InsertFullEmployee(SetEmployee(i));
+        //    }
+        //}
     }
 }
