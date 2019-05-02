b2bb83a1b15559d8a10425e986ea8e0f94a0fcb6
Matej Kardy Kardoš <matej.kardos@gmail.com>
2019-05-02 09:43:21 +0200

    Generating new data after some kardy made a mistake... Commenting back those methods.

--- AttendanceTermianal/AttendanceTermianal/Program.cs
+++ AttendanceTermianal/AttendanceTermianal/Program.cs
@@ -21,6 +21,9 @@ namespace AttendanceTermianal
             //var culture = new System.Globalization.CultureInfo("en-US");
             //Thread.CurrentThread.CurrentCulture = culture;
             //Thread.CurrentThread.CurrentUICulture = culture;
+            //EmployeeGenerator generator = new EmployeeGenerator();
+            //generator.GenerateSupervisor();
+            //generator.GenerateEmployee();
             CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en");
             CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en");
             Application.EnableVisualStyles();
