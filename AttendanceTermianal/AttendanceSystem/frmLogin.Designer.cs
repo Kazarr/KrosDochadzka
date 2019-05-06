c9701057b7000894c624eb469d9fdc318aecac40
JakubCelovsky <celovsky.jakubmail.com>
2019-05-06 15:45:26 +0200

    gui login-not final

--- AttendanceTermianal/AttendanceSystem/frmLogin.Designer.cs
+++ AttendanceTermianal/AttendanceSystem/frmLogin.Designer.cs
@@ -31,16 +31,18 @@
             this.buttonConfirm = new System.Windows.Forms.Button();
             this.labelLogin = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.buttonCancel = new System.Windows.Forms.Button();
             this.textBoxLogin = new System.Windows.Forms.TextBox();
             this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.btnChooseServer = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonConfirm
             // 
             this.buttonConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonConfirm.Location = new System.Drawing.Point(26, 227);
+            this.buttonConfirm.Location = new System.Drawing.Point(31, 267);
             this.buttonConfirm.Name = "buttonConfirm";
             this.buttonConfirm.Size = new System.Drawing.Size(154, 46);
             this.buttonConfirm.TabIndex = 0;
@@ -52,7 +54,7 @@
             // 
             this.labelLogin.AutoSize = true;
             this.labelLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.labelLogin.Location = new System.Drawing.Point(31, 125);
+            this.labelLogin.Location = new System.Drawing.Point(36, 205);
             this.labelLogin.Name = "labelLogin";
             this.labelLogin.Size = new System.Drawing.Size(149, 24);
             this.labelLogin.TabIndex = 1;
@@ -62,33 +64,24 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(31, 150);
+            this.label1.Location = new System.Drawing.Point(36, 230);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(106, 24);
             this.label1.TabIndex = 2;
             this.label1.Text = "Password:";
             // 
-            // buttonCancel
-            // 
-            this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonCancel.Location = new System.Drawing.Point(248, 227);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(154, 46);
-            this.buttonCancel.TabIndex = 3;
-            this.buttonCancel.Text = "Cancel";
-            this.buttonCancel.UseVisualStyleBackColor = true;
-            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
-            // 
             // textBoxLogin
             // 
-            this.textBoxLogin.Location = new System.Drawing.Point(219, 129);
+            this.textBoxLogin.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxLogin.Location = new System.Drawing.Point(201, 205);
             this.textBoxLogin.Name = "textBoxLogin";
-            this.textBoxLogin.Size = new System.Drawing.Size(167, 20);
+            this.textBoxLogin.Size = new System.Drawing.Size(201, 22);
             this.textBoxLogin.TabIndex = 4;
+            this.textBoxLogin.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Location = new System.Drawing.Point(219, 155);
+            this.textBoxPassword.Location = new System.Drawing.Point(224, 235);
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
             this.textBoxPassword.Size = new System.Drawing.Size(167, 20);
@@ -97,7 +90,7 @@
             // btnChooseServer
             // 
             this.btnChooseServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnChooseServer.Location = new System.Drawing.Point(26, 12);
+            this.btnChooseServer.Location = new System.Drawing.Point(12, 22);
             this.btnChooseServer.Name = "btnChooseServer";
             this.btnChooseServer.Size = new System.Drawing.Size(154, 46);
             this.btnChooseServer.TabIndex = 6;
@@ -105,21 +98,53 @@
             this.btnChooseServer.UseVisualStyleBackColor = true;
             this.btnChooseServer.Click += new System.EventHandler(this.btnChooseServer_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AttendanceSystem.Properties.Resources.ID;
+            this.pictureBox1.Location = new System.Drawing.Point(40, 346);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(376, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.White;
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnClose.Location = new System.Drawing.Point(393, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(32, 32);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 313);
+            this.BackColor = System.Drawing.Color.White;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(437, 415);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnChooseServer);
             this.Controls.Add(this.textBoxPassword);
             this.Controls.Add(this.textBoxLogin);
-            this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.labelLogin);
             this.Controls.Add(this.buttonConfirm);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "frmLogin";
             this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,9 +155,10 @@
         private System.Windows.Forms.Button buttonConfirm;
         private System.Windows.Forms.Label labelLogin;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.TextBox textBoxLogin;
         private System.Windows.Forms.TextBox textBoxPassword;
         private System.Windows.Forms.Button btnChooseServer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnClose;
     }
 }
