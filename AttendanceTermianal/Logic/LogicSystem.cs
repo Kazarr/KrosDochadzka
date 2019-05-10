69f195e3cbee557d03172158a9b21a2f74db1906
Stano Peťko <stano@stanopetko.eu>
2019-05-10 09:49:48 +0200

    Commit 4

--- AttendanceTermianal/Logic/LogicSystem.cs
+++ AttendanceTermianal/Logic/LogicSystem.cs
@@ -24,9 +24,9 @@ namespace Logic
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < hash.Length; i++)
             {
-                sb.Append(hash[i].ToString("X2"));
+                sb.Append(hash[i].ToString("x2"));
             }
-            return sb.ToString().ToLower();
+            return sb.ToString();
         }
 
         private DaySummary CreateDaySummary(DateTime date, int idEmployee)
