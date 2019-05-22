cd794e5cdef4948b71eb4ba79eae9f937e6a2d45
JakubCelovsky <37848658+qubo6@users.noreply.github.com>
2019-05-22 10:25:48 +0200

    Qubo solving issues (#124)
    * vyriešené #65
    časť obsluhy prešlo do viewmodelu a je tam o if menej
    
    * #63
    na forme už CorrectEmp nie je, len vo view modeli IsCorrectEmp
    
    * - white space
    
    * redukcia miesta
    
    * redukovanie z trm viewmodelu

--- AttendanceTermianal/TerminalViewModel.cs
+++ AttendanceTermianal/TerminalViewModel.cs
@@ -19,10 +19,11 @@ namespace AttendanceTermianal
         /// nastavenie dizajnu, rozne fonty pre rozne casove zlozky
         /// </summary>
         /// <returns></returns>
+
         public string DescriptionFullname(int employeeId)
         {
             return $"{_logic.GetPersonByIdEmployee(employeeId).FirstName} " +
-                                $"{_logic.GetPersonByIdEmployee(employeeId).LastName} ";
+                   $"{_logic.GetPersonByIdEmployee(employeeId).LastName} ";
         }
 
         /// <summary>
@@ -30,34 +31,16 @@ namespace AttendanceTermianal
         /// </summary>
         /// <param name="input"></param>
         /// <returns></returns>
-        public bool CorrectEmp(string input)
+        public bool IsCorrectEmp(string employeeId)
         {
             try
             {
-                var empoloyee = _logic.GetEmpolyeeByID(int.Parse(input));
-                return empoloyee != null && empoloyee.Id.Equals(int.Parse(input));
+                var empoloyee = _logic.GetEmpolyeeByID(int.Parse(employeeId));
+                return empoloyee != null && empoloyee.Id.Equals(int.Parse(employeeId));
             }
             catch (FormatException)
             {
                 return false;
-            }
-        }
-        
-        /// <summary>
-        /// Ak pre daného zamestnanca neexistuje žiaden záznam pre dnešný deň, vytvorím nový záznam
-        /// v ktorom mu nastavím start a finish time na aktuálny |||||DOVOD|||| - aby som nestratil čas odchodu,ak si zamestnanec nedal príchod
-        /// </summary>
-        /// <param name="employeeId"></param>
-        public void ExitDailyRecord(int employeeId)
-        {
-            DailyRecord dailyRecord = _logic.GetLastDailyRecordByEmployeeId(employeeId);
-            if (dailyRecord == null)
-            {
-                _logic.CreateNewTimeBlock(employeeId, EnumWorkType.Other, DateTime.Now, DateTime.Now);
-            }
-            else
-            {
-                _logic.UpdateFinishInTimeBlock(dailyRecord, DateTime.Now);
             }
         }
 
@@ -66,9 +49,16 @@ namespace AttendanceTermianal
         /// </summary>
         /// <param name="employeeId"></param>
         /// <param name="type"></param>
-        public void CreateNewAndFinishPreviousRecord(int employeeId, EnumWorkType type)
+        public void ProcessAction(int employeeId, EnumWorkType type)
         {
-            _logic.CreateNewAndFinishPreviousRecord(employeeId, type);
+            if (type == EnumWorkType.Exit)
+            {
+                _logic.ExitTimeBlock(employeeId);
+            }
+            else
+            {
+                _logic.CreateNewAndFinishPreviousRecord(employeeId, type);
+            }
         }
     }
 }
